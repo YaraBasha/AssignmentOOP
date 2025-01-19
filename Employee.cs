2446f9eb4e8572f21fffce4865dc06a988a6d3ea
Yara Hossam <141282551+YaraBasha@users.noreply.github.com>
2025-01-19 12:51:35 +0200

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace EmployeeApp
+{
+
+    public enum SecurityLevel
+    {
+        Guest,
+        Developer,
+        Secertary,
+        DBA
+    }
+
+    Public Class Employee
+{
+        public int ID { get; set; }
+    public string Name { get; set; }
+    public decimal Salary { get; set; }
+    public DateTime HireDate { get; set; }
+    private char _gender;
+    public SecurityLevel Security { get; set; }
+
+    /*public char Gender
+    {
+        get { return _gender; }
+        set
+        {
+            if (value == 'M' || value == 'F')
+            {
+                _gender = value;
+            }
+            else
+            {
+                throw new ArgumentException("Gender must be Male or Female");
+            }
+        }
+    }*/
+
+    public Employee(int id, string name, decimal salary, DateTime hireDate, char gender, SecurityLevel security)
+    {
+        ID = id;
+        Name = name;
+        Salary = salary;
+        HireDate = hireDate;
+        Gender = gender;
+        Security = security;
+    }
+
+    public override string ToString()
+    {
+        return String.Format(
+            "ID: {0}, Name: {1}, Salary: {2:C}, Hire Date: {3:d}, Gender: {4}, Security Level: {5}",
+            ID, Name, Salary, HireDate, Gender, SecurityPrivilege);
+    }
+
+}
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Console.WriteLine("Program Started");  
+
+
+        Employee emp1 = new Employee(1, "Yara Hossam", 6000.75m, new DateTime(2023, 2, 10), 'F', SecurityLevel.Secretary);
+        Employee emp2 = new Employee(2, "Sara Ahmed", 8000.90m, new DateTime(2020, 8, 25), 'F', SecurityLevel.DBA);
+        
+        
+        Console.WriteLine(emp1.ToString());
+        Console.WriteLine(emp2.ToString());
+        Console.ReadLine();
+
+    }
+}
